3c677222645f6af4d97d3b82d9d175354af693a7
Karina Assini Andreatta <karinaa@microsoft.com>
2025-01-07 16:22:54 -0300

    fix: fixing paths and small issues

--- src/deploy_ml_model/create_input_data_for_model.py
+++ src/deploy_ml_model/create_input_data_for_model.py
@@ -41,7 +41,7 @@ def create_input_data_for_model():
     # Create a data asset
     data_asset = Data(
         name="output_data_asset",
-        path=r"..\gbbai-o1-reasoning-over-ml\src\deploy-ml-model\data",
+        path=r"..\gbbai-o1-reasoning-over-ml\src\deploy_ml_model\data",
         type="uri_folder",
         description="Data asset for output data",
         tags={"source": "output"},
