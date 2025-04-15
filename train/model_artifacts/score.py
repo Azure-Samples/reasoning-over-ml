444f8de5b5c42bbeeaf9322774217f9e8704e053
Luiz Braz <luiz.braz@microsoft.com>
2025-04-15 14:19:37 -0300

    Add the explainer

--- train/model_artifacts/score.py
+++ train/model_artifacts/score.py
@@ -1,52 +1,64 @@
-import os, json
+import os
 import pickle
 import pandas as pd
-from typing import List
 
 
 def init():
     global model
+    global dtypes
+    global explainer
+    global columns
     
-    os.environ["AZUREML_MODEL_DIR"] = '/home/azureuser/cloudfiles/code/Users/lubraz/reasoning-over-ml/train/artifacts/'
-
     # Get the model path from the environment variable
-    model_path = os.environ["AZUREML_MODEL_DIR"]
-    model_path = os.path.join(model_path, "model.pkl")
+    root_path = os.path.join(os.environ["AZUREML_MODEL_DIR"], "model_artifacts")
+    model_path = os.path.join(root_path,  "model.pkl")
 
     # Load the model
     with open(model_path, 'rb') as f:
         model = pickle.load(f)
+
+    # Load the dtypes
+    dtypes_path = os.path.join(root_path, 'X_test.pkl')    
+    with open(dtypes_path, 'rb') as f:
+        dataset = pickle.load(f)
+        dtypes = dataset.dtypes.to_dict()
     
-    print('Model loaded successfully', model)
+    # Load the columns
+    columns = dataset.columns.tolist()
+    
+    # Load the shap explainer
+    explainer_path = os.path.join(root_path, 'explainer.pkl')
+    with open(explainer_path, 'rb') as f:
+        explainer = pickle.load(f)
+    
+    print('Model, dtypes and explainer loaded successfully')
    
 
 def run(mini_batch) -> pd.DataFrame:
-    results = []
+    results_predictions = []
+    results_shap_values = []
 
     print('Running the model')
-    print('Mini batch:', mini_batch)
     # Make predictions using the loaded model
-    try:
-        for record in mini_batch:
-            print('Record:', record)
-            data = pd.DataFrame(record['minibatch'])
-            
-            # For each data row make a prediction
-            for index, row in data.iterrows():
-                print('Processing row:', index)
-                predictions = model.predict(row)           
-                results.append(predictions.tolist())
+    try:    
+        data = pd.read_csv(mini_batch[0])
+        data = data.astype(dtypes)
+
+        # Predict using the loaded model
+        predictions = model.predict(data)
+        results_predictions.append(predictions.tolist())
+
+        # Get the SHAP values
+        shap_values = explainer.shap_values(data)
+        results_shap_values.append(shap_values.tolist())
+        
     except Exception as e:
         print(e)
         return str(e)
     
-    return pd.DataFrame(results)
+    # Combine the results into a single DataFrame
+    results = pd.DataFrame(shap_values, columns=columns)
+    results['prediction'] = predictions
 
-if __name__ == "__main__":
-    init()
-    run([{"id": 0, 
-         "dequeue_count": 3, 
-         "minibatch": ["/home/azureuser/cloudfiles/code/Users/lubraz/reasoning-over-ml/train/artifacts/X_test.csv"], 
-         "task_type": 4, 
-         "input_start_time": 1744658925.750685, 
-         "input_duration": 9.5367431640625e-07}])
+    
+    return results
